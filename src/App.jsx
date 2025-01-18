3256f14aa3faa55b9c8031beaab391282b688f8a
sumit-munda <sumitkmunda.563@gmail.com>
2025-01-18 09:56:50 +0000

    Implement Google and email/password authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import './index.css'; // Import global styles
-import HomePage from './pages/home/HomePage';
+import React from "react";
+import "./index.css"; // Import global styles
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// pages
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/login/LoginPage";
+import SignupPage from "./pages/signup/SignupPage";
+
+// import AppLayout from "./components/AppLayout";
+
+// define your routes
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+	{
+		path: "/login",
+		element: <LoginPage />,
+	},
+	{
+		path: "/signup",
+		element: <SignupPage />,
+	},
+	{
+		path: "*", // Fallback route for unknown paths
+		element: <div>Page Not Found</div>,
+	},
+
+	// app layout
+	// {
+	// 	path: "/",
+	// 	element: <AppLayout />,
+	// 	children: [
+	// 		{
+	// 			path: "/home",
+	// 			element: <HomePage />,
+	// 		},
+	// 		{ path: "/login", element: <LoginPage /> },
+	// 	],
+	// },
+]);
 
 const App = () => {
-  return (
-    <div className="App">
-      <HomePage />
-    </div>
-  );
+	return <RouterProvider router={router} />;
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,17 +2,29 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import { ToastContainer, toast } from "react-toastify";
+
+// images
 import domain from "./assets/images/domain.png";
 import logo from "./assets/images/logo.gif";
 import man from "./assets/images/man.png";
 import man2 from "./assets/images/man2.png";
 import woman from "./assets/images/woman.png";
 import qrcode from "./assets/images/qr-code.png";
+import tbrand from "./assets/images/triple-brand.png";
+
+// firebase
+import { FirebaseProvider } from "./context/firebase.jsx";
+
+// firebase
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<App />
+		<FirebaseProvider>
+			<App />
+			<ToastContainer />
+		</FirebaseProvider>
 	</StrictMode>
 );
 
-export { domain, logo, man, man2, woman, qrcode };
+export { domain, logo, man, man2, woman, qrcode, tbrand };
